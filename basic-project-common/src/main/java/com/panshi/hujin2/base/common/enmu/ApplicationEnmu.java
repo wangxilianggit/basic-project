d910c468f37429ecfeb198b6c441790b4d259fa4
wangxiliang <wangxiliang1991@outlook.com>
2019-05-16 09:26:24 +0800

    增加印尼国际化配置

--- basic-project-common/src/main/java/com/panshi/hujin2/base/common/enmu/ApplicationEnmu.java
+++ basic-project-common/src/main/java/com/panshi/hujin2/base/common/enmu/ApplicationEnmu.java
@@ -27,7 +27,7 @@ public enum ApplicationEnmu {
     BR_FREELOAN(14,"FreeLoan","pt_BR"),
 
     VI_CASH_DOG(21,"cashDog","vi"),
-    INA_WEB_GAME(22,"webGame","id-ID");// 印尼/印尼
+    INA_WEB_GAME(22,"webGame","id_ID");// 印尼/印尼
     ;;
 
     private Integer code;
