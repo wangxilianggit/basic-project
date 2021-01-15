e8b3b450104f301fa1470950869929f6ef351bd6
sjk405411511 <405411511@qq.com>
2021-01-15 15:33:00 +0800

    commit

--- basic-project-common/src/main/java/com/panshi/hujin2/base/common/enmu/ApplicationEnmu.java
+++ basic-project-common/src/main/java/com/panshi/hujin2/base/common/enmu/ApplicationEnmu.java
@@ -93,7 +93,7 @@ public enum ApplicationEnmu {
 
 
     //印度CPI
-    INDIA_CASH_KANGAROO(30001,"印度CPI","en_IN"),    // 中国  营销短信发送
+    INDIA_CASH_KANGAROO(30001,"CashKangaroo","en_IN"),    // 中国  营销短信发送
     ;
 
     private Integer code;
